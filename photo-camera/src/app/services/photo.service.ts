219b4459bc3a554905dd976f65eb80d63e5551b1
Revilo321 <58446652+Revilo321@users.noreply.github.com>
2023-09-15 11:02:20 +0200

    working

--- photo-camera/src/app/services/photo.service.ts
+++ photo-camera/src/app/services/photo.service.ts
@@ -1,9 +1,67 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Camera, CameraResultType, CameraSource, Photo} from '@capacitor/camera'
+import {Filesystem, Directory} from '@capacitor/filesystem'
+import {Preferences} from '@capacitor/preferences';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class PhotoService {
+    public photos : UserPhoto[] = [];
+    private PHOTO_STORAGE: string = 'photos';
+    constructor() {}
 
-  constructor() { }
+    private async readAsBase64(photo : Photo) { // Fetch the photo, read as a blob, then convert to base64 format
+        const response = await fetch(photo.webPath !);
+        const blob = await response.blob();
+        return await this.convertBlobToBase64(blob)as string;
+    }
+    private convertBlobToBase64 = (blob : Blob) => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+            resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+    });
+
+    private async savePicture(photo : Photo) {
+        const base64Data = await this.readAsBase64(photo);
+        // Write the file to the data directory
+        const fileName = Date.now() + '.jpeg';
+        await Filesystem.writeFile({path: fileName, data: base64Data, directory: Directory.Data});
+        return {filepath: fileName, webviewPath: photo.webPath}
+
+    }
+
+    public async loadSaved() {
+      // Retrieve cached photo array data
+      const { value } = await Preferences.get({ key: this.PHOTO_STORAGE });
+      this.photos = (value ? JSON.parse(value) : []) as UserPhoto[];
+      //
+      for (let photo of this.photos) {
+        // Read each saved photo's data from the Filesystem
+        const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+        });
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+       } 
+    }
+
+    public async addNewToGallery() { // Take a photo
+        const capturedPhoto = await Camera.getPhoto({resultType: CameraResultType.Uri, source: CameraSource.Camera, quality: 100});
+        const savedImageFile = await this.savePicture(capturedPhoto);
+        this.photos.unshift(savedImageFile)
+        Preferences.set({
+          key: this.PHOTO_STORAGE,
+          value: JSON.stringify(this.photos),
+         })
+    }
+
+
+}
+
+export interface UserPhoto {
+    filepath: string;
+    webviewPath?: string;
 }
