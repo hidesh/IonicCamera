f2d56bff31010c24ec8c531adc6cf2709a895abd
hidesh <hidesh@live.dk>
2023-09-15 12:12:29 +0200

    firebase added with login

--- photo-camera/src/environments/environment.prod.ts
+++ photo-camera/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDZGOpi-IzrVLEOVjasIamyc581D4U9M-w",
+    authDomain: "ionic-photo-292af.firebaseapp.com",
+    projectId: "ionic-photo-292af",
+    storageBucket: "ionic-photo-292af.appspot.com",
+    messagingSenderId: "391554675475",
+    appId: "1:391554675475:web:efa46c716532d30eac20e6",
+    measurementId: "G-FR71N57C9H"
+  },
 };
